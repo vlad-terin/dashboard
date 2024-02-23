e8fc52575ee935b3d402aaf66505339a690ec804
Vlad <vlad@mac.attlocal.net>
2024-02-23 16:33:46 -0600

    setting up db

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
+  "plugins": ["prettier-plugin-tailwindcss"],
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
